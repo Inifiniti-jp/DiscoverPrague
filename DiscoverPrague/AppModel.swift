fc8e321b0c7e9ee5b3b318b0fc6c5d59d6feda06
Your Name <you@example.com>
2025-01-26 13:05:47 +0100

    Initial Commit

---
+++ DiscoverPrague/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  DiscoverPrague
+//
+//  Created by Jakub Plachy on 26.01.2025.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ DiscoverPragueTests/DiscoverPragueTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverPragueTests.swift
+//  DiscoverPragueTests
+//
+//  Created by Jakub Plachy on 26.01.2025.
+//
+
+import Testing
+@testable import DiscoverPrague
+
+struct DiscoverPragueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
